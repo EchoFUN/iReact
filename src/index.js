79d339bc928ce68a4a09ecdc0e9e73a66ce1aad6
IamUncleXU <xukai.ken@gmail.com>
2022-10-08 15:40:08 +0800

    ADD

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+import { render } from './render';
+var h = require('virtual-dom/h');
+var createElement = require('virtual-dom/create-element')
+
+
+
+var count = 0;
+
+
+function createDiv(count) {
+  return h('div', {
+    style: {
+      textAlign: 'center',
+      lineHeight: (100 + count) + 'px',
+      border: '1px solid red',
+      width: (100 + count) + 'px',
+      height: (100 + count) + 'px'
+    }
+  }, [String(count)]);
+}
+
+var tree = createDiv(count);               // We need an initial tree
+var rootNode = createElement(tree);     // Create an initial root DOM node ...
+debugger
+document.body.appendChild(rootNode);
+
+
+
+
+render()
+
+
+
+
+
